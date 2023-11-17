2c50c2c9d4bcaa0cc7f87772e30551d35e3afeda
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-17 13:02:48 +0000

    Run tests for Blurbs views and information retrieval. Update testing.md

--- blurbs/tests.py
+++ blurbs/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Blurbs
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+# Tests run with Credit to Code Institute Run down of building tests in
+# Django Rest Framework Coursework Project.
+# Adapted for use in this API Project.
+
+class BlurbsViewTests(APITestCase):
+    def setUp(self):
+
+        # User from Django.
+        User.objects.create_user(username='ava', password='pass')
+
+    def test_can_list_blurbs(self):
+        ava = User.objects.get(username='ava')
+        Blurbs.objects.create(owner=ava, title='a title')
+        response = self.client.get('/blurbs/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_blurbs(self):
+        self.client.login(username='ava', password='pass')
+
+        # Pertaining to 'def post()'
+        response = self.client.post('/blurbs/', {'title': 'a title'})
+        count = Blurbs.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    # do not need to log in or fetch anything from the database like above.
+    # Pertaining to 'def post()'
+    # 200_OK for passing test.
+    def test_user_not_logged_in_cant_create_blurbs(self):
+        response = self.client.post('/blurbs/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class BlurbsInfoViewTests(APITestCase):
+    def setUp(self):
+        ava = User.objects.create_user(username='ava', password='pass')
+        david = User.objects.create_user(username='david', password='pass')
+        Blurbs.objects.create(
+            owner=ava, title='a title', content='ava content',
+            category='Spring'
+        )
+        Blurbs.objects.create(
+            owner=david, title='another title', content='davids content',
+            category='Spring'
+        )
+
+    def test_can_retrieve_blurbs_using_valid_id(self):
+        response = self.client.get('/blurbs/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_blurbs_using_invalid_id(self):
+        response = self.client.get('/blurbs/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    # Pass 201_CREATED code to make is fail.
+    # pass 404_NOT_FOUND code to make it pass.
+
+    def test_user_can_update_own_blurbs(self):
+        self.client.login(username='ava', password='pass')
+        response = self.client.put('/blurbs/1/', {'title': 'a new title'})
+        blurb = Blurbs.objects.filter(pk=1).first()
+        self.assertEqual(blurb.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # make it tail by adding a passible HTTP_200_OK.
+    def test_user_can_not_update_another_users_blurbs(self):
+        # force login
+        self.client.login(username='ava', password='pass')
+        response = self.client.put('/blurbs/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
--- testing.md
+++ testing.md
@@ -10,21 +10,38 @@ PEP8 Packages can be installed in the coding environment.<br>
 ## Extension.
 Extensions added to the workspace can help improve the experience of coding by assisting with debugging, Linters, formatting and much more. <br>
 Example of using an exntension.<br>
-<img src="assets/images/readme-images/pyex.png" width=30% height=30%><br>
+<img src="assets/images/testing-images/pyex.png" width=30% height=30%><br>
 <br>
 Example of using an extention.<br>
-<img src="assets/images/readme-images/flake8.png" width=20% height=20%><br>
+<img src="assets/images/testing-images/flake8.png" width=20% height=20%><br>
 <br>
 [Flake8](https://flake8.pycqa.org/en/latest/) and [Python Linting Extentions](https://code.visualstudio.com/docs/python/linting) based extensions can be installed to help to perfect the look of a coding environment by providing linting support for [Python](https://www.python.org/) code.<br>
 <br>
 'Problems' tab within the coding workspace can be used to suggest problems within the code to be fixed with use of an extension.<br>
 Example showing whitespace that needs to be removed and unused imports.<br>
-<img src="assets/images/readme-images/problemstab.png" width="50%" height="50%">
+<img src="assets/images/testing-images/problemstab.png" width="50%" height="50%">
 <br>
 'Problems' tab can identify many other issues thoughout building a website.<br>
 
 <hr>
 
+## Django Rest Framework Testing
+
+[Django Rest Framework](https://www.django-rest-framework.org/) offers testing libraries in built to test code. <br>
+APITestCase needs to be imported from rset framework tests.<br>
+CLI entry: **python manage.py test**<br>
+<br>
+Shown in image below. Tests show if they have run succesfully after being tested in the code to fail firstly using appropriate HTTP status codes.<br>
+<img src="assets/images/testing-images/tests1.png" width="50%" height="50%">
+<br>
+<br>
+Tests can be set to fail depending on the HTTP status code. This image depicts a test failing, and the code needed for it to pass.<br>
+<img src="assets/images/testing-images/tests1.png" width="50%" height="50%">
+<br>
+<br>
+They need to be set up to run a test against. Each class contains (APITestCase) in the class and each function starts with 'test'<br>
+<img src="assets/images/testing-images/test2.png" width="50%" height="50%">
+<br>
 
 <br>
 
