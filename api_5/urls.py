37ff9c7df606b3a523d3dac89a802e8a241586f1
ajorgensen89 <ajorgensen89@gmail.com>
2023-11-19 13:12:58 +0000

    log our route suggested fix

--- api_5/urls.py
+++ api_5/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
     # Logout route above to be matched first
-    # path('dj-rest-auth/logout/', logout_route),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
        'dj-rest-auth/registration/',
